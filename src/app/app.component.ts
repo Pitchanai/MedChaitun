06f83f998b120c28a28d73f4aa82147b01c8e2e8
Pitchanai Thitipakorn <pitchanai.t@cdg.co.th>
2020-04-23 18:13:44 +0700

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core'
+import * as Chart from 'chart.js'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('pieChart') pieChartRef: ElementRef
+
+  title = 'app'
+  textCitizenId: any = ''
+  citizenIdLength = 0
+  showChart = false
+  pieChart = null
+  chartData = {
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [],
+        borderColor: [],
+        borderWidth: 1,
+      },
+    ],
+    labels: [],
+  }
+  private chartRedrawConfig = {
+    duration: 1000,
+    easing: 'easeOutQuart',
+    lazy: false,
+  }
+
+  getDigitcount() {
+    let idLength = 0
+    if (!this.textCitizenId) {
+      return ''
+    }
+
+    let textCitizenIdString = this.textCitizenId.toString()
+    if (textCitizenIdString) {
+      idLength = textCitizenIdString.length
+    }
+
+    this.citizenIdLength = idLength
+
+    if (this.citizenIdLength > 13) {
+      return '(เกิน)'
+    } else if (this.citizenIdLength == 13) {
+      return '(ครบ)'
+    }
+
+    return `(ขาด ${13 - this.citizenIdLength} หลัก)`
+  }
+
+  isDisableCalculate() {
+    return !(this.citizenIdLength == 13)
+    // return true
+  }
+
+  submitCalculate() {
+    if (!this.textCitizenId) {
+      console.log('validate 1 error')
+      return
+    }
+
+    let citizenId = this.textCitizenId
+    let scoreTotal = []
+
+    for (let i = 1; i < 1000; i++) {
+      let j = i / 10
+      let score = Math.round((citizenId * 10000) / j) % 10000
+      scoreTotal.push(score)
+    }
+
+    const range = 250
+    const rangeMax = 10000
+    let labelTotal = []
+    let rangeTotal = []
+
+    for (let i = 0; i < rangeMax; i = i + range) {
+      labelTotal.push(`[${i},${i+range})`)
+      rangeTotal.push(scoreTotal.filter((x) => x >= i && x < i + range).length)
+    }
+
+    console.log(labelTotal)
+    console.log(rangeTotal)
+
+    this.showChart = true
+
+    let canvas = this.pieChartRef.nativeElement
+    let ctx = canvas.getContext('2d')
+    this.pieChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labelTotal,
+        datasets: [
+          {
+            label: 'Score Distribution',
+            data: rangeTotal,
+            // backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+            // borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+            borderWidth: 1
+          },
+        ],
+      },
+    })
+    // this.pieChart.update(this.chartRedrawConfig)
+  }
 }
