91075219f6bb6cc0a8aab23cbff36b0ae80db840
Pitchanai Thitipakorn <pitchanai.t@cdg.co.th>
2020-04-23 18:17:49 +0700

    final color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -98,6 +98,8 @@ export class AppComponent {
           {
             label: 'Score Distribution',
             data: rangeTotal,
+            backgroundColor: 'rgba(54, 162, 235, 0.6)',
+            borderColor: 'gba(54, 162, 235, 1)',
             // backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
             // borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
             borderWidth: 1
